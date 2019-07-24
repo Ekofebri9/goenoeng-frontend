dfb62ebeff2fa0035ccee03e355ce5e5e329236a
hajidito <hajidito.kerja@gmail.com>
2019-07-24 15:06:43 +0700

    update

--- src/route/AppNavigation.js
+++ src/route/AppNavigation.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
-import {createAppContainer, createBottomTabNavigator ,createStackNavigator} from 'react-navigation';
+import {createAppContainer, createBottomTabNavigator ,createSwitchNavigator,createStackNavigator} from 'react-navigation';
 import Dummy from '../screens/dummy'
 import MountainDetail from '../screens/MountainDetail'
 import BookingMountain from '../screens/BookingMountain'
 import Chat from '../screens/Chat'
+import Login from '../screens/Login';
+import SignUp from '../screens/Index';
+import AuthLoading from '../screens/AuthLoading';
+import User from '../screens/User';
+import Mitra from '../screens/Mitra';
 
 const BottomNavigation = createBottomTabNavigator(
 	{
     Dummy: Dummy,
     MountainDetail:MountainDetail,
-    BookingMountain:BookingMountain,
-	  Chat:Chat
-	},
+  },
 	{
         
     }
@@ -20,24 +23,66 @@ const BottomNavigation = createBottomTabNavigator(
 
 const AppNavigator = createStackNavigator(
   {
-    Dummy: {
-      screen: BottomNavigation
-    },
     MountainDetail: {
       screen: BottomNavigation
     },
     BookingMountain:{
-      screen: BottomNavigation
+      screen: BookingMountain
     },
     Chat:{
+      screen: Chat
+    },
+    User:{
+      screen: User
+    },
+    Mitra:{
+      screen: Mitra
+    },
+    Dummy: {
       screen: BottomNavigation
-    }
+    },
+    // Login: {
+    //   screen: Login
+    // },
+    // SignUp: {
+    //   screen: SignUp
+    // },
   }, 
   {
     headerMode: 'none',
     navigationOptions: {
+      header: null,
       headerVisible: false,
     }
 });
 
-export default createAppContainer(AppNavigator);
+const AuthStack = createStackNavigator({
+  Login: {
+    screen: Login
+  },
+});
+const RegisterStack = createStackNavigator({
+  SignUp: {
+    screen: SignUp
+  },
+  User:{
+    screen: User
+  },
+  Mitra:{
+    screen: Mitra
+  },
+});
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    // SplashScreen: SplashScreen,
+    AuthLoading: AuthLoading,
+    App: AppNavigator,
+    Register: RegisterStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+));
+// export default createAppContainer(AppNavigator);
